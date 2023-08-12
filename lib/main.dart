81fa8aab5cc11e8bc7dfbed3f17682e6e8195563
HyunsangJoo <joohs86@naver.com>
2023-08-12 16:43:58 +0900

    Splash Screen WIP
    Added an asset (yaml)
    StatelessWidget,
    Column widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,25 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.black87,
-        body: Center(
-          child: Text('Hello Flutter',
-          style: TextStyle(
+  runApp(MaterialApp(home: HomeScreen()));
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset(
+            'asset/img/logo.png',
+          ),
+          CircularProgressIndicator(
             color: Colors.white,
-            fontSize: 20.0,
           ),
-          ),
-        ),
+        ],
       ),
-    ),
-  );
+    );
+  }
 }
